112723ccdf8c601ce9c829e7d98ee56e36829d4c
soshi1822 <soshi1822@gmail.com>
2020-11-19 16:32:34 +0900

    タイル出力をするように

--- src/index.js
+++ src/index.js
@@ -7,8 +7,15 @@ class GPVTile {
     constructor(option) {
         this.maxZoom = option.maxZoom ?? 9;
         this.minZoom = option.minZoom ?? 3;
-        this.start = [lat2pint(option.startLatitude, this.maxZoom), lon2pint(option.startLongitude, this.maxZoom)];
-        this.end = [lat2pint(option.endLatitude, this.maxZoom), lon2pint(option.endLongitude, this.maxZoom)];
+        this.dir = option.dir;
+        this.start = [
+            toTileSize(lat2pint(option.startLatitude, this.maxZoom), false),
+            toTileSize(lon2pint(option.startLongitude, this.maxZoom), false)
+        ];
+        this.end = [
+            toTileSize(lat2pint(option.endLatitude, this.maxZoom), true),
+            toTileSize(lon2pint(option.endLongitude, this.maxZoom), true)
+        ];
         this.canvas = canvas_1.createCanvas(this.end[1] - this.start[1], this.end[0] - this.start[0]);
         this.ctx = this.canvas.getContext('2d');
     }
@@ -24,12 +31,45 @@ class GPVTile {
         this.ctx.fillStyle = color;
         this.ctx.fillRect(start[1] - this.start[1], start[0] - this.start[0], end[1] - start[1], end[0] - start[0]);
     }
-    export() {
+    export(dir) {
         this.ctx.save();
-        fs_1.writeFileSync('./out.png', this.canvas.toBuffer('image/png', { compressionLevel: 0 }));
+        this.output(this.maxZoom, this.start, this.end);
+    }
+    output(zoom, start, end) {
+        if (zoom < this.minZoom) {
+            return;
+        }
+        const dir = `${this.dir}/${zoom}`;
+        fs_1.mkdirSync(dir, { recursive: true });
+        const xStart = start[1] / 256;
+        const yStart = start[0] / 256;
+        const xEnd = end[1] / 256;
+        const yEnd = end[0] / 256;
+        for (let x = xStart; x <= xEnd; x++) {
+            const xPx = (x - xStart) * 256;
+            for (let y = yStart; y <= yEnd; y++) {
+                const yPx = (y - yStart) * 256;
+                const canvas = canvas_1.createCanvas(256, 256);
+                const ctx = canvas.getContext('2d');
+                ctx.drawImage(this.canvas, xPx, yPx, 256, 256, 0, 0, 256, 256);
+                fs_1.writeFileSync(`${dir}/${x}_${y}.png`, canvas.toBuffer('image/png'));
+            }
+        }
+        const halfStart = [toTileSize(start[0] / 2, false), toTileSize(start[1] / 2, false)];
+        const halfEnd = [toTileSize(end[0] / 2, true), toTileSize(end[1] / 2, true)];
+        const canvas = canvas_1.createCanvas(halfEnd[1] - halfStart[1], halfEnd[0] - halfStart[0]);
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(this.canvas, 0, 0, end[1] - start[1], end[0] - start[0], start[1] / 2 - halfStart[1], start[0] / 2 - halfStart[0], (end[1] - start[1]) / 2, (end[0] - start[0]) / 2);
+        this.canvas = canvas;
+        this.ctx = ctx;
+        this.output(zoom - 1, halfStart, halfEnd);
     }
 }
 exports.GPVTile = GPVTile;
+function toTileSize(num, blend = false) {
+    return (~~(num / 256) + (blend ? 1 : 0)) * 256;
+}
+;
 function lat2pint(lat, zoom) {
     return ~~(2 ** (zoom + 7) / Math.PI * (-Math.atanh(Math.sin(Math.PI / 180 * lat)) + Math.atanh(Math.sin(Math.PI / 180 * 85.05112878))));
 }
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,27 @@
 import { Canvas, CanvasRenderingContext2D, createCanvas } from 'canvas';
-import { writeFileSync } from 'fs';
+import { mkdirSync, writeFileSync } from 'fs';
 
 export class GPVTile {
   private readonly start: [number, number];
   private readonly end: [number, number];
   private readonly maxZoom: number;
   private readonly minZoom: number;
-  private readonly canvas: Canvas;
-  private readonly ctx: CanvasRenderingContext2D;
+  private readonly dir: string;
+  private canvas: Canvas;
+  private ctx: CanvasRenderingContext2D;
 
-  constructor(option: { startLatitude: number, startLongitude: number, endLatitude: number, endLongitude: number, maxZoom?: number, minZoom?: number }) {
+  constructor(option: { startLatitude: number, startLongitude: number, endLatitude: number, endLongitude: number, maxZoom?: number, minZoom?: number, dir: string }) {
     this.maxZoom = option.maxZoom ?? 9;
     this.minZoom = option.minZoom ?? 3;
-    this.start = [lat2pint(option.startLatitude, this.maxZoom), lon2pint(option.startLongitude, this.maxZoom)];
-    this.end = [lat2pint(option.endLatitude, this.maxZoom), lon2pint(option.endLongitude, this.maxZoom)];
+    this.dir = option.dir;
+    this.start = [
+      toTileSize(lat2pint(option.startLatitude, this.maxZoom), false),
+      toTileSize(lon2pint(option.startLongitude, this.maxZoom), false)
+    ];
+    this.end = [
+      toTileSize(lat2pint(option.endLatitude, this.maxZoom), true),
+      toTileSize(lon2pint(option.endLongitude, this.maxZoom), true)
+    ];
     this.canvas = createCanvas(this.end[1] - this.start[1], this.end[0] - this.start[0]);
     this.ctx = this.canvas.getContext('2d');
   }
@@ -34,8 +42,56 @@ export class GPVTile {
 
   export() {
     this.ctx.save();
-    writeFileSync('./out.png', this.canvas.toBuffer('image/png', { compressionLevel: 0 }));
+    this.output(this.maxZoom, this.start, this.end);
   }
+
+  private output(zoom: number, start: [number, number], end: [number, number]) {
+    if (zoom < this.minZoom) {
+      return;
+    }
+
+    const dir = `${this.dir}/${zoom}`;
+
+    mkdirSync(dir, { recursive: true });
+
+    const xStart = start[1] / 256;
+    const yStart = start[0] / 256;
+    const xEnd = end[1] / 256;
+    const yEnd = end[0] / 256;
+
+    for (let x = xStart; x <= xEnd; x++) {
+      const xPx = (x - xStart) * 256;
+      for (let y = yStart; y <= yEnd; y++) {
+        const yPx = (y - yStart) * 256;
+
+        const canvas = createCanvas(256, 256);
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(this.canvas, xPx, yPx, 256, 256, 0, 0, 256, 256);
+        writeFileSync(`${dir}/${x}_${y}.png`, canvas.toBuffer('image/png'));
+      }
+    }
+
+    const halfStart: [number, number] = [toTileSize(start[0] / 2, false), toTileSize(start[1] / 2, false)];
+    const halfEnd: [number, number] = [toTileSize(end[0] / 2, true), toTileSize(end[1] / 2, true)];
+
+    const canvas = createCanvas(halfEnd[1] - halfStart[1], halfEnd[0] - halfStart[0]);
+    const ctx = canvas.getContext('2d');
+
+    ctx.drawImage(
+      this.canvas, 0, 0, end[1] - start[1], end[0] - start[0],
+      start[1] / 2 - halfStart[1], start[0] / 2 - halfStart[0], (end[1] - start[1]) / 2, (end[0] - start[0]) / 2
+    );
+
+    this.canvas = canvas;
+    this.ctx = ctx;
+
+    this.output(zoom - 1, halfStart, halfEnd);
+  }
+
+}
+
+function toTileSize(num: number, blend = false) {
+  return (~~(num / 256) + (blend ? 1 : 0)) * 256;
 }
 
 function lat2pint(lat: number, zoom: number) {
